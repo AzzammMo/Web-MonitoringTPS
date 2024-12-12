a0da6de23290c55bd3f777be9076b5625b6dcd4b
Azzam <adinsjhay10@gmail.com>
2024-12-13 01:03:06 +0700

    second commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class, // Tambahkan middleware role di sini
-        
     ];
 
     /**
